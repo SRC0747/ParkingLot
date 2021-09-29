208567c29824a1bf516ad9e650494d9178a7a357
SRC0747 <RjSrc@0747>
2021-09-30 00:56:30 +0530

    Added: Driver want to Unpark Car so that he can go home.

--- src/test/java/com/parkingslot/ParkingLotTest.java
+++ src/test/java/com/parkingslot/ParkingLotTest.java
@@ -1,16 +1,38 @@
 package com.parkingslot;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class ParkingLotTest {
+    ParkingLotSystem parkingLotSystem = null;
+    Object vehicle = null;
+
+    @BeforeEach
+    public void setUp() throws Exception {
+        vehicle = new Object();
+        parkingLotSystem = new ParkingLotSystem();
+    }
 
     @Test
     public void givenAVehicle_WhenParked_ShouldReturnTrue() {
-        ParkingLotSystem parkingLotSystem = new ParkingLotSystem();
         boolean isParked = parkingLotSystem.park(new Object());
         Assertions.assertTrue(isParked);
+    }
+
+    @Test
+    public void givenAVehicle_WhenAlreadyParked_ShouldReturnFalse() {
+        parkingLotSystem.park(vehicle);
+        boolean isParked = parkingLotSystem.park(new Object());
+        Assertions.assertFalse(isParked);
+    }
+
+    @Test
+    public void givenAVehicle_WhenUnParked_ShouldReturnTrue() {
+        parkingLotSystem.park(vehicle);
+        boolean isUnParked = parkingLotSystem.unPark(vehicle);
+        Assertions.assertTrue(isUnParked);
     }
 }
