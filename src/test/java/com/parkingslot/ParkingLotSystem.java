acb651bf337c930ad4bc18bb5e8ee5fafcb84d00
SRC0747 <RjSrc@0747>
2021-10-04 14:06:05 +0530

    Added: Showing Sign of parkingLot is Full for Multiple Car Parking.

--- src/test/java/com/parkingslot/ParkingLotSystem.java
+++ src/test/java/com/parkingslot/ParkingLotSystem.java
@@ -1,42 +1,51 @@
 package com.parkingslot;
 
+import java.util.ArrayList;
+
 public class ParkingLotSystem {
-    private final int actualCapacity;
-    private  int currentCapacity;
-    private Object vehicle;
+    private int actualCapacity;
+    //private  int currentCapacity;
+    private ArrayList vehicles;
     private ParkingLotOwner owner;
 
     public ParkingLotSystem(int capacity){
-        this.currentCapacity = 0;
+        this.vehicles = new ArrayList<>();
+        //this.currentCapacity = 0;
+        this.actualCapacity = capacity;
+    }
+
+    public void registerOwner(ParkingLotOwner owner) {
+        this.owner = owner;
+    }
+
+    public void setCapacity(int capacity) {
         this.actualCapacity = capacity;
     }
 
     public void park(Object vehicle) throws ParkingLotException{
-        if (this.currentCapacity == this.actualCapacity) {
+        if (this.vehicles.size() == this.actualCapacity) {
             owner.capacityIsFull();
             throw new ParkingLotException("ParkingLot is full.");
         }
-        this.currentCapacity++;
-        this.vehicle = vehicle;
+        //this.currentCapacity++;
+        this.vehicles.add(vehicle);
     }
 
     public boolean unPark(Object vehicle) {
         if (vehicle == null)
             return false;
-        if (this.vehicle.equals(vehicle)){
-            this.vehicle = null;
+        if (this.vehicles.contains(vehicle)){
+            //this.vehicle = null;
+            this.vehicles.remove(vehicle);
             return true;
         }
         return false;
     }
 
     public boolean isVehicleParked(Object vehicle) {
-        if (this.vehicle.equals(vehicle))
+        if (this.vehicles.contains(vehicle))
             return true;
         return false;
     }
 
-    public void registerOwner(ParkingLotOwner owner) {
-        this.owner = owner;
-    }
 }
